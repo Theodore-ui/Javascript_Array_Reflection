19e7465a87f31b420e8002c07c514669b9c802a4
Theodore-ui <theodore.lorenc@netmatters-scs.com>
2023-02-16 12:13:36 +0000

    The gernerted image can now be associated with an email and all the images accoited with an email can be displlayed when the email is inputted

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 // random image display
-
 const card = document.querySelector('#image_container');
 const imgBtn = document.querySelector('#refresh_img');
 
@@ -11,6 +10,16 @@ const emailRegex = /^[_\.0-9a-zA-Z-]+@([0-9a-zA-Z][0-9a-zA-Z-]+\.)+[a-zA-Z]{2,6}
 const emailNullMsg = "Please enter your email";
 const emailFormatMsg = "Please enter a valid email";
 const emailSuccessMsg = "Success! your email has been linked to the image";
+const repeatImgMsg = "Image is aleardy Linked to email Address";
+//email and image shorage
+let db = []
+
+//Image search
+const search = document.querySelector('#search_input');
+const searchMsg = document.querySelector("#search_msg");
+const searchBtn = document.querySelector("#search_submit");
+const imageGallery = document.querySelector('#image_gallery');
+const searchNotFoundMsg = "Email address not recognised";
 
 //Fetch functions
 
@@ -52,35 +61,87 @@ function changeImage() {
     fetchData('https://picsum.photos/200') 
         .then(data => {
             img.src = data.url;
-            img.alt = 'Random Image r';
+            img.alt = 'Random Image';
         })
 }
 
 //validate email
 
 function validateEmail() {
+    const img = card.querySelector('img'); 
     if (email.value === '') {
-        html = emailNullMsg;
-         
+        html = emailNullMsg;      
     } else if (!email.value.match(emailRegex)) {
         html = emailFormatMsg;
     } else {
-        html = emailSuccessMsg;
-        
+        pushToDB(email.value, img.src)
+        html = emailSuccessMsg;    
     }
     emailMsg.textContent = html;
 }
 
+// function checkImageRepeats() {
+//     let imageExists;
+//     for (let j = 0; j < db[i].images.length; j++) {
+//         if (image === db[i].images[j]) {
+//             imageExists = true;
+//         } else {
+//             imageExists = false;
+//             pushToDB (email.value, img.src)    
+//         }
+//     }
+//     if (!imageExists) {
+//         return true
+//     }
+// }
+
 //pust object to databass
 
-//email and image shorage
+function pushToDB(emailA, image) {
+    let emailExists;
+    for (let i = 0; i < db.length; i++) {
+        if (db[i].email === emailA) {
+            db[i].images.push(image)
+            emailExists = true;
+            break;
+        } else {
+            emailExists = false;
+        }
+    }
+    if (!emailExists) {
+        db.push({email: email.value, images: [image]});
+    }
+}
 
-db = []
+
+//retrieve images 
+
+function retrieveImages() {
+    let searchExists;
+    for (let i = 0; i < db.length; i++) {
+        if (db[i].email === search.value) {
+            imageGallery.innerHTML = '';
+            for (let j = 0; j < db[i].images.length; j++) {
+                let html = `
+                <img src='${db[i].images[j]}'>
+                `;
+                imageGallery.innerHTML += html;
+            }
+            searchExists = true;
+            break;
+        } else {
+            searchExists = false;
+        }
+    }
+    if (!searchExists) {
+        searchMsg.textContent = searchNotFoundMsg;    
+    }  
+}
 
 //Event Listeners
-emailBtn.addEventListener('click', validateEmail);
-
 imgBtn.addEventListener('click', changeImage);
+emailBtn.addEventListener('click', validateEmail);
+searchBtn.addEventListener('click', retrieveImages);
 
 
 
