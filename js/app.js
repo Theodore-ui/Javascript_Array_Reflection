f77adcc1cfa2018dcb02c698ff05a6621d967665
Theodore-ui <theodore.lorenc@netmatters-scs.com>
2023-02-21 09:42:33 +0000

    I have added the ability to remove images from a gallery by clicking on them

--- js/app.js
+++ js/app.js
@@ -1,17 +1,23 @@
+//colours amd boxshadows
+
+const $darkGreen = '#25a25a';
+const $greenBoxShadow = 'rgba(37, 162, 90, 0.25) 0px 54px 55px, rgba(37, 162, 90, 0.12) 0px -12px 30px, rgba(37, 162, 90, 0.12) 0px 4px 6px, rgba(37, 162, 90, 0.17) 0px 12px 13px, rgba(37, 162, 90, 0.09) 0px -3px 5px';
+const $blueBoxShadow = 'rgba(42, 110, 198, 0.25) 0px 54px 55px, rgba(42, 110, 198, 0.12) 0px -12px 30px, rgba(42, 110, 198, 0.12) 0px 4px 6px, rgba(42, 110, 198, 0.17) 0px 12px 13px, rgba(42, 110, 198, 0.09) 0px -3px 5px';
 // random image display
 const card = document.querySelector('#image_container');
 const imgBtn = document.querySelector('.refresh_img');
-const url = 'https://picsum.photos/405'; 
+const url = 'https://picsum.photos/405';
+
 
 //email validation
 const email = document.querySelector('#email_input');
 const emailMsg = document.querySelector("#input_msg");
 const emailBtn = document.querySelector(".email_submit");
 const emailRegex = /^[_\.0-9a-zA-Z-]+@([0-9a-zA-Z][0-9a-zA-Z-]+\.)+[a-zA-Z]{2,6}$/;
-const emailNullMsg = "Please enter your email";
+const emailNullMsg = "Please enter an email";
 const emailFormatMsg = "Please enter a valid email";
 const emailSuccessMsg = "Success! your email has been linked to the image";
-const repeatImgMsg = "Image is aleardy Linked to email Address";
+const repeatImgMsg = "Image is aleardy Linked to email";
 const pod = document.querySelector('.link_img');
  
 //email and image shorage
@@ -21,7 +27,7 @@ let db = [];
 const search = document.querySelector('#search_input');
 const searchMsg = document.querySelector("#search_msg");
 const searchBtn = document.querySelector(".search_submit");
-const imageGallery = document.querySelector('#image_gallery');
+const imageGallery = document.querySelector('.image_gallery');
 const searchNotFoundMsg = "Email address not recognised";
 const imageGalleryCont = document.querySelector('#image_gallery_container');
 const exitBtn = document.querySelector('.exit');
@@ -71,17 +77,21 @@ function changeImage() {
 
 //validate email
 
-function validateEmail() {
-    const img = card.querySelector('img'); 
+function validateEmail() { 
+    const img = card.querySelector('img');
     if (email.value === '') {
-        html = emailNullMsg;      
+        html = emailNullMsg; 
+        emailMsg.style.color = 'red';     
     } else if (!email.value.match(emailRegex)) {
         html = emailFormatMsg;
+        emailMsg.style.color = 'red';
     } else if (checkImageRepeats(email.value, img.src) === true) {
-        html = repeatImgMsg; 
+        html = repeatImgMsg;
+        emailMsg.style.color = 'red'; 
     } else {
-        pushToDB(email.value, img.src);
-        html = emailSuccessMsg;   
+        html = emailSuccessMsg; 
+        emailMsg.style.color = $darkGreen; 
+        pushToDB(email.value, img.src); 
     }
     emailMsg.textContent = html;
     
@@ -109,12 +119,16 @@ function checkImageRepeats(emailIn, image) {
 //pust object to databass
 
 function pushToDB(emailIn, image) {
-
+    const img = imageGallery.querySelector('img');
     let emailExists;
     for (let i = 0; i < db.length; i++) {
         if (db[i].email === emailIn) {
             db[i].images.push(image)
             emailExists = true;
+            if (imageGallery.classList.contains(emailIn)) {
+                retrieveImages()   
+            }
+
             break;
         } else {
             emailExists = false;
@@ -128,14 +142,14 @@ function pushToDB(emailIn, image) {
 
 function pushToDBAnimations() {
     pod.style.boxShadow = 'none';
-    pod.style.boxShadow = 'rgba(37, 162, 90, 0.25) 0px 54px 55px, rgba(37, 162, 90, 0.12) 0px -12px 30px, rgba(37, 162, 90, 0.12) 0px 4px 6px, rgba(37, 162, 90, 0.17) 0px 12px 13px, rgba(37, 162, 90, 0.09) 0px -3px 5px';
+    pod.style.boxShadow = $greenBoxShadow;
     emailBtn.style.display = 'none';
     setTimeout( function() {
         changeImage()
         pod.style.boxShadow = 'rgba(0, 0, 0, 0.35) 0px 5px 15px';
         emailMsg.textContent = '';
         emailBtn.style.display = 'block';
-    },2000);
+    },1500);
 }
 
 //retrieve images 
@@ -148,25 +162,39 @@ function retrieveImages() {
             imageGallery.innerHTML = '';
             for (let j = 0; j < db[i].images.length; j++) {
                 let html = `
-                <img src='${db[i].images[j]}' alt='random image'>
+                <img src='${db[i].images[j]}' alt='${db[i].email}'>
                 `;
                 imageGallery.innerHTML += html;
+                imageGallery.className = '';
+                imageGallery.classList.add('image_gallery', search.value);
             }
-            pod.style.boxShadow = 'none';
-            pod.style.boxShadow = 'rgba(42, 110, 198, 0.25) 0px 54px 55px, rgba(42, 110, 198, 0.12) 0px -12px 30px, rgba(42, 110, 198, 0.12) 0px 4px 6px, rgba(42, 110, 198, 0.17) 0px 12px 13px, rgba(42, 110, 198, 0.09) 0px -3px 5px';
-            setTimeout( function() {
-                changeImage()
-                pod.style.boxShadow = 'rgba(0, 0, 0, 0.35) 0px 5px 15px';
-            },2000);
+            for (i of imageGallery.querySelectorAll('img')) {
+                i.addEventListener('click', removeImage);
+            }
             searchExists = true;
+            retrieveImagesAnimations()
             break;
         } else {
             searchExists = false;
         }
     }
     if (!searchExists) {
+        searchMsg.style.color = 'red';
         searchMsg.textContent = searchNotFoundMsg;    
-    }  
+    }
+    setTimeout( function() {
+        changeImage()
+        searchMsg.textContent = '';
+    },1500); 
+}
+
+function retrieveImagesAnimations() {
+    pod.style.boxShadow = 'none';
+    pod.style.boxShadow = $blueBoxShadow;
+    setTimeout( function() {
+        changeImage()
+        pod.style.boxShadow = 'rgba(0, 0, 0, 0.35) 0px 5px 15px';
+    },2000);
 }
 
 //closing the gallery 
@@ -175,7 +203,21 @@ function closeGallery () {
     imageGalleryCont.style.display = 'none';
 }
 
+//removing an image from the gallery
+
+function removeImage(e) {
+    e.target.remove();
+}
+
+// if (imageGallery.innerHTML = '') {
+//     imageGalleryCont.style.di
+// }
+
 //Event Listeners
+
+    
+
+
 imgBtn.addEventListener('click', changeImage);
 emailBtn.addEventListener('click', validateEmail);
 searchBtn.addEventListener('click', retrieveImages);
